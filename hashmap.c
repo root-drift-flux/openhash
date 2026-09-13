3234981c767fa2c37370900f619d2c024751be37
Wyatt <187879781+root-drift-flux@users.noreply.github.com>
2026-09-13 11:31:37 -0500

    add hm_put & hm_get. valgrind clean

--- hashmap.c
+++ hashmap.c
@@ -3,8 +3,19 @@
 #include <string.h>
 #include "hashmap.h"
 
+static size_t hash_key(const char *key)
+{
+    size_t hash = 14695981039346656037UL;   // FNV offset basis (64-bit) 
+    for (const char *p = key; *p; p++) {
+        hash ^= (unsigned char)*p;           // XOR in the byte 
+        hash *= 1099511628211UL;             // multiply by FNV prime 
+    }
 
-hashmap *hm_create(){
+    return hash;
+}
+
+hashmap *hm_create()
+{
     
     hashmap *h = malloc(sizeof(hashmap));
     if (h == NULL) {
@@ -22,16 +33,66 @@ hashmap *hm_create(){
     return h;
 }
 
-void hm_free(hashmap *h){
+void hm_free(hashmap *h)
+{
     if (h == NULL){
         return;
     }
-        for (size_t i = 0; i < h->capacity; i++){
-            if (h->slots[i].state == SLOT_OCCUPIED) {
+
+    for (size_t i = 0; i < h->capacity; i++){
+        if (h->slots[i].state == SLOT_OCCUPIED) {
                 free(h->slots[i].key);
         }
     }
+
     free(h->slots);
     free(h);
     
 }
+
+int hm_put(hashmap *h, const char *key, int value) 
+{
+	size_t index = hash_key(key) % h->capacity; // add key 
+
+    while (h->slots[index].state == SLOT_OCCUPIED) {   // box taken?
+        if (strcmp(h->slots[index].key, key) == 0) {   // by this same key?
+            h->slots[index].value = value;             // update, done
+            return 0;
+        }
+        
+        index = (index + 1) % h->capacity;             // else walk to next box
+    }
+
+    h->slots[index].key = strdup(key);
+    if (h->slots[index].key == NULL) {
+        return -1;
+    }
+
+    h->slots[index].value = value;
+    h->slots[index].state = SLOT_OCCUPIED;
+    h->count++;
+    return 0;
+}
+
+int hm_get(hashmap *h, const char *key, int *out_value)
+{
+   size_t index = hash_key(key) % h->capacity;
+
+   while (h->slots[index].state != SLOT_EMPTY){
+        if (h->slots[index].state == SLOT_OCCUPIED &&
+            strcmp(h->slots[index].key, key) == 0){
+            *out_value = h->slots[index].value;
+            return 0;
+        }
+
+        index = (index + 1) % h->capacity;
+   }
+    
+   return -1;
+}
+
+
+
+
+
+
--- main.c
+++ main.c
@@ -1,9 +1,31 @@
 #include "hashmap.h"
-
+#include <stdio.h>
 
 int main(void){
     
     hashmap *h= hm_create();
+    
+    hm_put(h, "cat", 3);
+    hm_put(h, "dog", 7);
+    hm_put(h, "bird", 42);
+
+    int value;
+
+    if (hm_get(h, "dog", &value) == 0)
+        printf("dog = %d\n", value);
+    else
+        printf("dog not found\n");
+
+    if (hm_get(h, "cat", &value) == 0)
+        printf("cat = %d\n", value);
+    else
+        printf("cat not found\n");
+
+    if (hm_get(h, "fish", &value) == 0)
+        printf("fish = %d\n", value);
+    else
+        printf("fish not found\n");
+
     hm_free(h);
     return 0;
 
